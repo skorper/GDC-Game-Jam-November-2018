bf46124edaa4633f9efa02437accf61146048a33
Lauren-McKim <mckiml@nmsu.edu>
2018-11-10 16:36:34 -0500

    add menu to exit volume and pause game

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class menuManager : MonoBehaviour {
+    public OptionsPanel optionsPanel;
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            optionsPanel.gameObject.SetActive(!optionsPanel.gameObject.activeSelf);
+        }
+        if (optionsPanel.gameObject.activeSelf)
+        {
+            Time.timeScale = 0;
+        }
+        else
+        {
+            Time.timeScale = 1;
+        }
+		
+	}
+}
---
+++ Assets/Scripts/OptionsPanel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionsPanel : MonoBehaviour {
+    public Button exitGameButton;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ExitGame()
+    {
+        Debug.Log("Trying to exit game will not work in editor.");
+        Application.Quit();
+    }
+}

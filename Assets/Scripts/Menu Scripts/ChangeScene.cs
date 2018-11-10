cabdf39a266e527b4d33be9c202c2521fac96810
Greg De La Torre <gdlt5@nmsu.edu>
2018-11-10 03:28:37 -0700

    Added comments to ChangeScene and ChangeHealthStatus Scripts

--- Assets/Scripts/Menu Scripts/ChangeScene.cs
+++ Assets/Scripts/Menu Scripts/ChangeScene.cs
@@ -1,13 +1,29 @@
-﻿using System.Collections;
+﻿/*
+	ChangeScene.cs
+	Written By: Greg De La Torre
+	Last Modified: November 2018
+
+	Class allows user to assign a scene to be switched to when method OnMouseUp is called.
+	Input: Scene Name
+	Output: Scene Transition
+*/
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ChangeScene : MonoBehaviour {
 
+	/* Declare empty string for input in development environment */
 	public string levelToLoad = "";
 
+	/* 
+		OnMouseUp Method
+		Method transitions from current scene to specified scene
+		Parameters: N/A
+		Output: Scene
+	*/
 	public void OnMouseUp()
 	 {
 	     Application.LoadLevel(levelToLoad); 
-	 }
-}
+	 } /* End OnMouseUp */
+}/* End Class */
--- Assets/Scripts/UI Scripts/ChangeHealthStatus.cs
+++ Assets/Scripts/UI Scripts/ChangeHealthStatus.cs
@@ -1,22 +1,40 @@
-﻿using System.Collections;
+﻿/*
+	ChangeHealthStatus.cs
+	Written By: Greg De La Torre
+	Last Modified: November 2018
+
+	Class changes image displayed in UI to indicate health level.
+	Input: Sprites, GameObject
+	Output: Sprite
+*/
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ChangeHealthStatus : MonoBehaviour {
 
-	 public Sprite fullHealth;
-	 public Sprite midHealth;
-	 public Sprite lowHealth;
-	 public Sprite noHealth;
-	 public GameObject player;
-	 protected int healthFlag = 0;
+	/* Declare sprites needed */
+	public Sprite fullHealth;
+	public Sprite midHealth;
+	public Sprite lowHealth;
+	public Sprite noHealth;
+
+	/* Declare player object */
+	public GameObject player;
+
+	/* Declare integer flag to determine if image change is necessary */
+	protected int healthFlag = 0;
 
 
+	/* Check player health every update */
 	void Update(){
 
+		/* If player health hasn't changed, don't change image */
 		if(player.GetComponent<PlayerController>().Health == healthFlag)
 			return;
 
+		/* If player health has changes, changed image based on health level */
 	 	if(player.GetComponent<PlayerController>().Health == 3){
 	 		healthFlag = 3;
 	 		this.transform.GetComponent<UnityEngine.UI.Image>().sprite = fullHealth;
@@ -29,11 +47,11 @@ public class ChangeHealthStatus : MonoBehaviour {
 	 		healthFlag = 1;
 	  		this.transform.GetComponent<UnityEngine.UI.Image>().sprite = lowHealth;
 	  	}
+
+	  	/* If player health is 0, remove image */
 	  	else{
 	  		healthFlag = 0;
 	  		this.transform.GetComponent<UnityEngine.UI.Image>().sprite = noHealth;
 	  	}
-
-
-	}
-}
+	} /* End Update */
+} /* End Class */

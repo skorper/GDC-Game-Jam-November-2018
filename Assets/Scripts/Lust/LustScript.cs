8119a8c390482979a6e94a1e2126aa61b7b9dcf4
Jeffrey <lansfordjr14@gmail.com>
2018-11-10 06:05:34 -0700

    Added walk to Lust when he/she gets below 3

--- Assets/Scripts/Lust/LustScript.cs
+++ Assets/Scripts/Lust/LustScript.cs
@@ -26,8 +26,16 @@ public class LustScript : MonoBehaviour {
 
     public GameObject player;
 
-	// Use this for initialization
-	void Start () {
+    private float distance;
+
+    private Vector2 moveDistanceVector;
+
+    private Rigidbody2D rig;
+
+   
+
+    // Use this for initialization
+    void Start () {
 
         health = 6;
 
@@ -39,10 +47,14 @@ public class LustScript : MonoBehaviour {
 
         player = GameObject.FindGameObjectWithTag("Player");
 
-	}
+        rig = GetComponent<Rigidbody2D>();
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
+
+        
 
         if (tempTime > 0) {
 
@@ -57,23 +69,16 @@ public class LustScript : MonoBehaviour {
 
         }
 
+        if ( health <= 3 ) {
+            Walk();
+
+        }
+
     }
 
     public void Kiss() {
 
-        Instantiate(kiss, new Vector3(transform.position.x - 2, transform.position.y, 0), Quaternion.identity );
-
-    }
-
-    public void SpikeFloor () {
-
-
-    }
-    
-    public void Kick () {
-
-        
-
+        Instantiate(kiss, new Vector3(transform.position.x - 2, transform.position.y, 0), Quaternion.identity);
 
     }
 
@@ -87,6 +92,24 @@ public class LustScript : MonoBehaviour {
     }
 
     public void Walk () {
+
+        player = GameObject.FindGameObjectWithTag("Player");
+
+        moveDistanceVector = new Vector2(transform.position.x - player.transform.position.x, transform.position.y - player.transform.position.y);
+
+        distance = moveDistanceVector.magnitude;
+
+
+            if (moveDistanceVector.x > 0) {
+
+                rig.velocity = new Vector2(-moveSpeed, rig.velocity.y);
+            }
+
+            if (moveDistanceVector.x < 0) {
+
+                rig.velocity = new Vector2(moveSpeed, rig.velocity.y);
+
+            }
 
 
     }

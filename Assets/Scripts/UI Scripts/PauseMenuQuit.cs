12fda47324323a94a947c90d85dbe62a2b43c767
Greg De La Torre <gdlt5@nmsu.edu>
2018-11-10 05:24:42 -0700

    Fixed Pause Menu Functionality
    Fixed tediousness of having to define transition for each button per scene

---
+++ Assets/Scripts/UI Scripts/PauseMenuQuit.cs
@@ -0,0 +1,27 @@
+﻿/*
+	PauseMenuQuit.cs
+	Written By: Greg De La Torre
+	Last Modified: November 2018
+
+	Class allows user to leave to main menu when method OnClick is called.
+	Input: Scene Name
+	Output: Scene Transition
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenuQuit : MonoBehaviour {
+
+	/* 
+		OnClick Method
+		Method transitions from current scene to specified scene
+		Parameters: N/A
+		Output: Scene
+	*/
+	public void OnClick()
+	{
+		Application.LoadLevel("mainmenu"); 
+	} /* End OnMouseUp */
+}/* End Class */
---
+++ Assets/Scripts/UI Scripts/PauseMenuRestart.cs
@@ -0,0 +1,27 @@
+﻿/*
+	PauseMenuRestart.cs
+	Written By: Greg De La Torre
+	Last Modified: November 2018
+
+	Class allows user to assign a scene to be switched to when method OnClick is called.
+	Input: Scene Name
+	Output: Scene Transition
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenuRestart : MonoBehaviour {
+
+	/* 
+		OnClick Method
+		Method transitions from current scene to specified scene
+		Parameters: N/A
+		Output: Scene
+	*/
+	public void OnClick()
+	{
+		Application.LoadLevel(Application.loadedLevel); 
+	} /* End OnMouseUp */
+}/* End Class */

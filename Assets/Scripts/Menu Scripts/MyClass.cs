2baa0db103803146b324950e5ef9c972e62e6ab0
Jeff <jeffreyinocente@gmail.com>
2018-11-09 23:47:19 -0700

    added main menu
    Main menu scene with working quit button. Needs assets

---
+++ Assets/Scripts/Menu Scripts/MyClass.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class MyClass : MonoBehaviour
+{
+
+    public void Quit()
+    {
+        UnityEditor.EditorApplication.isPlaying = false;
+
+        Application.Quit();
+    }
+}

d62c8b09be2748efa94b4455a9ee4d13abe876ad
Greg De La Torre <gdlt5@nmsu.edu>
2018-11-10 02:57:57 -0700

    Added Health UI Elements
    Health display image changes based on player health from 0-3. Needs assets.

---
+++ Assets/Scripts/UI Scripts/ChangeHealthStatus.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeHealthStatus : MonoBehaviour {
+
+	 public Sprite fullHealth;
+	 public Sprite midHealth;
+	 public Sprite lowHealth;
+	 public Sprite noHealth;
+	 public GameObject player;
+	 protected int healthFlag = 0;
+
+
+	void Update(){
+
+		if(player.GetComponent<PlayerController>().Health == healthFlag)
+			return;
+
+	 	if(player.GetComponent<PlayerController>().Health == 3){
+	 		healthFlag = 3;
+	 		this.transform.GetComponent<UnityEngine.UI.Image>().sprite = fullHealth;
+	 	}
+	 	else if(player.GetComponent<PlayerController>().Health == 2){
+	 		healthFlag = 2;
+	  		this.transform.GetComponent<UnityEngine.UI.Image>().sprite = midHealth;
+	  	}
+	 	else if(player.GetComponent<PlayerController>().Health == 1){
+	 		healthFlag = 1;
+	  		this.transform.GetComponent<UnityEngine.UI.Image>().sprite = lowHealth;
+	  	}
+	  	else{
+	  		healthFlag = 0;
+	  		this.transform.GetComponent<UnityEngine.UI.Image>().sprite = noHealth;
+	  	}
+
+
+	}
+}

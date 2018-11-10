dc3ace745517bc4fbb5c0f726aa21fe446b9b1c5
Greg De La Torre <gdlt5@nmsu.edu>
2018-11-10 01:58:19 -0700

    Added settings menu, updated main menu
    Main menu now quits and enters test opening scene, as well as settings menu. Main needs assets and to be routed to correct opening scene. Settings need to be tied together.

---
+++ Assets/Scripts/Menu Scripts/ChangeScene.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeScene : MonoBehaviour {
+
+	public string levelToLoad = "";
+
+	public void OnMouseUp()
+	 {
+	     Application.LoadLevel(levelToLoad); 
+	 }
+}

717ee04630e8addf0b4524555baf3bb03f31d5de
Greg De La Torre <gdlt5@nmsu.edu>
2018-11-10 17:20:23 -0700

    Added Pride Animation Controller
    Needs completion and application.

---
+++ Assets/Scripts/Menu Scripts/MusicToggle.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicToggle : MonoBehaviour {
+
+	
+	public void musicToggle(){
+
+		AudioSource audioSource = GetComponent<AudioSource>();
+
+
+		audioSource.mute = !audioSource.mute;
+
+		DontDestroyOnLoad(audioSource);
+	}
+}
